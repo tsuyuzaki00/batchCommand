57fe355201ffc9d3213eddd42912cb28bf674522
tsuyuzakitatsuya <tsuyuzaki.tatsuya@engi-st.jp>
2020-12-24 16:18:01 +0900

    ファイルのみ設定で実行が可能になった

--- testImageCreate.bat
+++ testImageCreate.bat
@@ -1,7 +1,5 @@
 @echo off
 
-"C:\Program Files\Autodesk\Maya2020\bin\mayabatch.exe" -file "D:\Maya\scenes\test.ma" -command "python(\"from mainEdit import pngRenderImage as ps;reload(ps); ps.save()\")"
-"C:\Program Files\Autodesk\Maya2020\bin\Render.exe" -r "sw" -cam "shotCam_front" -rd "D:\Maya\images" "D:\Maya\scenes\test.ma"
-"C:\Program Files\Autodesk\Maya2020\bin\mayabatch.exe" -file "D:\Maya\scenes\test.ma" -command "python(\"from mainEdit import pngRenderImage as ps;reload(ps); ps.delete()\")"
+"C:\Program Files\Autodesk\Maya2020\bin\mayabatch.exe" -file "D:\Maya\scenes\test.ma" -command "python(\"from mainEdit import pngRenderImage as ps;reload(ps); ps.main()\")"
 
 pause
